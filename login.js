074ef683b4f9652247be113ef38b2e7502b4d439
Armaanpreet Kaur <armaanpreet0855.becse24@chitkara.edu.in>
2025-04-25 18:19:33 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const loginForm = document.querySelector(".form-container:first-child");
+	const signupForm = document.querySelector(".form-container:last-child");
+	const showLoginLink = document.getElementById("show-login");
+	const showSignupLink = document.getElementById("show-signup");
+  });
